f1336ee3fdd1f24703fbde9e791ae8b6f510b934
cnTerence <cn_terence@163.com>
2014-09-28 17:36:56 +0800

    update

---
+++ src/vargas/dgsd22/android/ListAdapter.java
@@ -0,0 +1,45 @@
+package vargas.dgsd22.android;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListAdapter extends BaseAdapter {
+	
+	List<Map<String, String>> listData = new ArrayList<Map<String,String>>();
+	private LayoutInflater inflater;
+	
+	
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+
+}
---
+++ src/vargas/dgsd22/android/ListItemViewHolder.java
@@ -0,0 +1,10 @@
+package vargas.dgsd22.android;
+
+import android.widget.TextView;
+
+public class ListItemViewHolder {
+
+	public TextView tvRight;
+	public TextView tvTop;
+	public TextView tvBottom;
+}
